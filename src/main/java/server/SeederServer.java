c7ee9a9e6eddaa8cc02a08b86166e971cda8b1ea
artfer <artureco95.af@gmail.com>
2019-10-29 17:19:42 +0000

    Imprimiu um puta dum 0!

--- src/main/java/server/SeederServer.java
+++ src/main/java/server/SeederServer.java
@@ -6,27 +6,7 @@ import java.util.LinkedList;
 
 public class SeederServer {
 
-    private LinkedList<String> seeders;
-    private LinkedList<String> seedingTo;
-    private boolean seeding = (seedingTo.size() > 0);
 
-    public String getSeeder(){
-        return seeders.getFirst();
-    }
-
-    public void removeSeeder(String seeder){
-        seeders.remove(seeder);
-        // if seeders.size == 0
-        // warn client
-        // else
-        // switch seeder
-        //      warn seeder
-
-    }
-
-    public void switchSeeder(){
-
-    }
 
 
 
